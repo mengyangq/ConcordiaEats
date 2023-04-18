e9ec655145c3aaa16936d739745d6949a5c16972
Yihang H <huangyihang1992@gmail.com>
2023-04-17 21:55:26 -0400

    Re-organize

--- src/main/java/com/comp5541/ConcordiaEats/repository/ProductRepository.java
+++ src/main/java/com/comp5541/ConcordiaEats/repository/ProductRepository.java
@@ -1,16 +1,13 @@
 package com.comp5541.ConcordiaEats.repository;
 
-import com.comp5541.ConcordiaEats.model.Product;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-//import org.springframework.data.repository.query.Param;
-//import org.springframework.stereotype.Repository;
-import java.util.List;
-
+import com.comp5541.ConcordiaEats.model.Product;
 
 //@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
